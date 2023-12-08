7eda4e1b341cef105ef6f17c6c6c1be60f8dae29
Максим Ледовский <santech_montage@mail.ru>
2023-12-08 19:58:43 +0300

    -

--- Structure_file/Structure_file.cpp
+++ Structure_file/Structure_file.cpp
@@ -2,13 +2,16 @@
 #include <windows.h>
 #include <fstream>
 #include <string>
+#include <iomanip>
+#include <conio.h>
 
 using namespace std;
 
 #define PATH1 "data/input.txt"
 #define PATH2 "data/output.txt"
+#define N 100
 
-int main()
+int main_t()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
@@ -22,52 +25,52 @@ int main()
 		return 1;
 	}
 
-	string inf = "";
-	unsigned short int i = 0, k = 0;
-	cout << "Начинаем запись информации о изделиях (Нажмите '*' для завершения ввода)" << endl;
-	do
+	unsigned short int i = 0, count = 0;
+	cout << "Начинаем запись информации об изделиях" << endl;
+	for (count; count < N; count++)
 	{
+		unsigned short int t1; // выделяем память для локальных переменных ввода из консоли и дальнейшего переноса в текстовый файл для дальнейшего считывания
+		char t2[25];
+
 		if (i == 5)
 		{
 			file << '\n';
 			i = 0;
-			k++;
+			count++;
 		}
 		switch (i)
 		{
 		case 0:
 			cout << "Введите Артикул: ";
+			cin >> t2;
+			file << t2;
 			break;
 		case 1:
 			cout << "Введите Наименование: ";
+			cin >> t2;
+			file << t2;
 			break;
 		case 2:
 			cout << "Введите Стоимость: ";
+			cin >> t1;
+			file << t1;
 			break;
 		case 3:
 			cout << "Введите Количество: ";
+			cin >> t1;
+			file << t1;
 			break;
 		case 4:
 			cout << "Введите Дату: ";
+			cin >> t2;
+			file << t2;
 			break;
 		}
-		cin >> inf;
-		file << inf << ' ';
-	} 
-	while (inf != "*");
-	
+
+	}
+
 	cout << "Запись в файл успешна завершена!" << endl;
 	file.close();
-
-	struct data_base
-	{
-		unsigned short int art;
-		char name[20];
-		unsigned long int cost;
-		unsigned long int count;
-		char date[8];
-	}
-	pr; // pr (product) - товар
 
 	ifstream in(PATH1);
 	if (in.is_open())
@@ -77,7 +80,7 @@ int main()
 		cout << "Файл для чтения исходных данных не открыт!" << endl;
 		return 1;
 	}
-	
+
 	ofstream out(PATH2);
 	if (out.is_open())
 		cout << "Файл для записи результатов успешно открыт!" << endl;
@@ -86,32 +89,37 @@ int main()
 		cout << "Файл для записи результатов не открыт!" << endl;
 		return 1;
 	}
+	out << setw(25) << "Артикул" << '\t|\t' << setw(25) << "Наименование" << '\t|\t' << setw(25) << "Стоимость" << '\t|\t' << setw(25) << "Количество" << '\t|\t' << setw(26) << "Дата производства";
 
-	unsigned long int mnc, mxk;
+	struct data_base
+	{
+		char art[25];
+		char name[20];
+		unsigned long int cost;
+		unsigned long int count;
+		char date[8];
+	}
+	pr; // pr (product) - товар
+
+	unsigned long int mnc, mxk; // mnc (min cost) - наименьшая цена, mxk (max count) - наибольшее количество
+
+	in >> pr.art >> pr.name >> pr.cost >> pr.count >> pr.date; // считываем первый товар и берём начальные значения цены и количества, с помощью которых будет сравнивать другие товары
+	mnc = pr.cost;
+	mxk = pr.count;
+	out << setw(25) << pr.art << '\t|\t' << setw(25) << pr.name << '\t|\t' << setw(25) << pr.cost << '\t|\t' << setw(25) << pr.count << '\t|\t' << setw(25) << pr.date;
 
 	while (in.eof())
-		for (int k = 0; k < 5; k++)
-		{
-			getline(in, inf, ' ');
-			switch (i)
-			{
-			case 0:
-				pr.art = stoi(inf);
-				break;
-			case 1:
-				strcpy_s(pr.name, inf.c_str());
-				break;
-			case 2:
-				pr.cost = stoi(inf);
-				break;
-			case 3:
-				pr.count = stoi(inf);
-				break;
-			case 4:
-				strcpy_s(pr.date, inf.c_str());
-				break;
-			}
-		}
+	{
+		in >> pr.art >> pr.name >> pr.cost >> pr.count >> pr.date;
+		if (pr.cost < mnc && pr.count > mxk)
+			out << setw(25) << pr.art << '\t|\t' << setw(25) << pr.name << '\t|\t' << setw(25) << pr.cost << '\t|\t' << setw(25) << pr.count << '\t|\t' << setw(25) << pr.date;
 
+	}
+
+	cout << "Работа с файлом успешно завершена!";
+	in.close();
+	out.close();
+
+	_getch();
 	return 0;
 }
